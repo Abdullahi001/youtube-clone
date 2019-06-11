ab531f003af5c17e1eab66deb744d6312ee181d9
Abdullahi Musa <abdullahimusa4991@gmail.com>
2019-06-11 20:16:27 +0300

    defaulting video selection

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,15 +3,10 @@ import React from "react";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div
-      onClick={() => {
-        onVideoSelect(video);
-      }}
-      className="video-item item"
-    >
+    <div onClick={() => onVideoSelect(video)} className="video-item item">
       <img
+        alt={video.snippet.title}
         className="ui image"
-        alt="video_image"
         src={video.snippet.thumbnails.medium.url}
       />
       <div className="content">
